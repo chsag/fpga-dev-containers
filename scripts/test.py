81a84a841ccc9e19341f4134ba46bc992d8f1f5a
seimsel <seimsel@gmail.com>
2021-01-25 09:31:06 +0100

    Automatically find all files.

--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,23 @@
 from importlib.util import spec_from_file_location, module_from_spec
-from glob import glob
+from glob import iglob
+from itertools import chain
 from vunit import VUnit
 
 if __name__ == '__main__':
     test_environment = VUnit.from_argv()
     test_lib = test_environment.add_library('test')
 
-    test_files = glob('./test/test_*.py')
+    for f in chain(
+        iglob('./external/**/src/**/*.vhd', recursive=True),
+        iglob('./src/**/*.vhd', recursive=True),
+        iglob('./test/**/*.vhd', recursive=True)
+    ):
+        test_lib.add_source_file(f)
 
-    for path in test_files:
+    for path in iglob('./test/test_*.py'):
         spec = spec_from_file_location('test', path)
         test = module_from_spec(spec)
         spec.loader.exec_module(test)
-        test.add_tests(test_environment, test_lib)
+        test.add_configs(test_lib)
 
     test_environment.main()
