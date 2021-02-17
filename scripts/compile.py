ebf5bd713497e2d810f0aaaa164a3f4891a3f48c
seimsel <seimsel@gmail.com>
2021-02-17 11:08:11 +0100

    Inject version from tag.

--- scripts/compile.py
+++ scripts/compile.py
@@ -2,32 +2,29 @@
 from subprocess import run
 from glob import glob
 from os import rename, remove, getenv
+from os.path import exists
 from shutil import copyfile
 from pathlib import Path
+from re import search
 
 project_path = Path(glob('/work/*.qpf')[0])
 settings_path = project_path.with_suffix('.qsf')
 
-env = [
-    'VERSION_MAJOR',
-    'VERSION_MINOR',
-    'VERSION_VARIANT',
-    'VERSION_REVISION'
-]
+ref = getenv('Build.SourceBranch', '')
 
-copyfile(settings_path, settings_path.with_suffix('.tmp'))
+try:
+    version_major, version_minor, version_revision = search(r'v(\d+)\.(\d+)\.(\d+)', ref).groups()
 
-with open(settings_path, 'a') as settings_file:
-    def inject_env(name, f):
-        value = getenv(name)
+    copyfile(settings_path, settings_path.with_suffix('.tmp'))
 
-        if value == None:
-            return
+    with open(settings_path, 'a') as settings_file:
+        settings_file.write(f'\nset_parameter -name VERSION_MAJOR {version_major}')
+        settings_file.write(f'\nset_parameter -name VERSION_MINOR {version_minor}')
+        settings_file.write(f'\nset_parameter -name VERSION_VARIANT 1')
+        settings_file.write(f'\nset_parameter -name VERSION_REVISION {version_revision}')
 
-        f.write(f'\nset_parameter -name {name} {value}')
-
-    for name in env:
-        inject_env(name, settings_file)
+except AttributeError:
+    pass
 
 run([
     '/opt/intelFPGA_lite/20.1/quartus/bin/quartus_sh',
@@ -36,5 +33,6 @@ run([
     str(project_path)
 ], check=True)
 
-remove(settings_path)
-rename(settings_path.with_suffix('.tmp'), settings_path)
+if exists(settings_path.with_suffix('.tmp')):
+    remove(settings_path)
+    rename(settings_path.with_suffix('.tmp'), settings_path)
