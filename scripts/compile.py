433e58654bbd810d9fbe63c14c28b4f2e3e329c5
seimsel <seimsel@gmail.com>
2021-02-17 13:22:31 +0100

    Rename build output.

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from subprocess import run
 from glob import glob
-from os import rename, remove, getenv
+from os import rename, makedirs, remove, getenv
 from os.path import exists
 from shutil import copyfile
 from pathlib import Path
@@ -12,8 +12,10 @@ settings_path = project_path.with_suffix('.qsf')
 
 ref = getenv('Build.SourceBranch', '')
 
+makedirs('build')
+
 try:
-    version_major, version_minor, version_revision = search(r'v(\d+)\.(\d+)\.(\d+)', ref).groups()
+    version_major, version_minor, version_revision, version_rc = search(r'v(\d+)\.(\d+)\.(\d+)_rc(\d+)', ref).groups()
 
     copyfile(settings_path, settings_path.with_suffix('.tmp'))
 
@@ -24,7 +26,10 @@ try:
         settings_file.write(f'\nset_parameter -name VERSION_REVISION {version_revision}')
 
 except AttributeError:
-    pass
+    version_major = None
+    version_minor = None
+    version_revision =None
+    version_rc = None
 
 run([
     '/opt/intelFPGA_lite/20.1/quartus/bin/quartus_sh',
@@ -36,3 +41,8 @@ run([
 if exists(settings_path.with_suffix('.tmp')):
     remove(settings_path)
     rename(settings_path.with_suffix('.tmp'), settings_path)
+
+binary_path = Path(glob('/work/output_files/*.rbf')[0])
+
+if version_major != None:
+    rename(binary_path, f'/work/build/{binary_path.stem}_v{version_major}_{version_minor}_{version_revision}_rc_{version_rc}.rbf')
