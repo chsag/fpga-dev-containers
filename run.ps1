e247ed5dc99b49ae72ce2ab0e9202ad27732598c
simonzumbrunnen <simon.zumbrunnen@soudronic.com>
2022-01-03 13:28:57 +0100

    Removed old files. Only container in this repo.

--- run.ps1
+++
@@ -1,9 +0,0 @@
-$script, $arguments = $args
-
-docker run --rm --tty `
-    --volume "${PWD}:/work" `
-    --workdir "/work" `
-    chsag/vhdl-scripts:0.3 `
-    python3 `
-    "/work/scripts/scripts/$script.py" `
-    $arguments
